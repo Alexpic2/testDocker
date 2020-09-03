74efb303da111f62482406973a36fc3f94f5698f
alpic64 <alexandre.picard.5@ulaval.ca>
2020-09-03 08:19:39 -0400

    volume test

--- hello world/entrypoint.sh
+++ hello world/entrypoint.sh
@@ -1,7 +1,14 @@
 echo 'start hello test'
 
+#mkdir export
+cp ./helloTest ./export
+# validation of cp
+echo '$ ls -l -a '
+ls -l -a
+echo '$ ls -l -a export/ '
+ls -l -a export/
+
 ./helloTest
 
-#ajouter le copy vers un dossier /export dans le fichier .sh
 
 #créer un volume et copier
