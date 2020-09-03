d06683c09c4049cb15163ae5ddfbdad97446de2d
APicard <alex.picard2@gmail.com>
2020-09-03 14:14:32 -0400

    volume can output the file on ubuntu

--- hello world/entrypoint.sh
+++ hello world/entrypoint.sh
@@ -1,12 +1,17 @@
 echo 'start hello test'
 
 #mkdir export
-cp ./helloTest ./export
+cp ./helloTest /usr/src/buildvts/export/
 # validation of cp
+echo 'pwd'
+pwd
 echo '$ ls -l -a '
 ls -l -a
 echo '$ ls -l -a export/ '
 ls -l -a export/
+echo '$ ls -l -a /usr/src/buildvts/export/ '
+ls -l -a /usr/src/buildvts/export/
+
 
 ./helloTest
 
